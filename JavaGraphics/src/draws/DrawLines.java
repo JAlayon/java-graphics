23198fc455070781110f0500a493598bd5b70385
Jair Alayon <jair.atl.95@hotmail.com>
2017-07-12 12:24:15 -0500

    lineas
    se agregan como dibujar lineas

---
+++ JavaGraphics/src/draws/DrawLines.java
@@ -0,0 +1,23 @@
+
+package draws;
+
+import java.awt.Canvas;
+import java.awt.Graphics;
+
+public class DrawLines extends Canvas {
+
+    @Override
+    public void paint(Graphics g) {
+        int width = getWidth();
+        int height = getHeight();
+        
+        //vertical
+        g.drawLine(width/2, 0, width/2, height);
+        //horizontal
+        g.drawLine(0, height/2, width, height/2);
+        //cruzados
+        g.drawLine(0, 0, width, height);
+        g.drawLine(0, height, width, 0);
+    }
+    
+}
---
+++ JavaGraphics/src/draws/MainDraws.java
@@ -0,0 +1,25 @@
+
+package draws;
+
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+
+public class MainDraws {
+    
+    static final int WIDTH = 600;
+    static final int HEIGHT = 500;
+    static final String TITTLE = "Dibujos 2D";
+    static DrawLines lines = new DrawLines();
+    
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(WIDTH, HEIGHT);
+        frame.setTitle(TITTLE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setLayout(new BorderLayout());
+        
+        frame.add(lines);
+        frame.setVisible(true);
+    }
+}
